e697bfb5078f4a778e5efbe4284588a46962a308
m-jim-d <jim.miller.tc@gmail.com>
2025-01-17 14:35:48 -0600

    file change

--- amend.bat
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -m "a new message"
-git push --force-with-lease origin main
--- publish.bat
+++
@@ -1,7 +0,0 @@
-@ECHO off
-
-:: script for publishing python files to Github repository (...)
-
-git add .
-git commit -am "file change"
-git push origin main
